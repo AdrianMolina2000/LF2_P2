f244ab245b207cfb85895f9d0288ecbd8b920428
Adrian Molina <adriansmc@gmail.com>
2020-10-29 13:45:59 -0600

    commit de emergencia

---
+++ prueba.py
@@ -0,0 +1,16 @@
+# doble = [[0,1,2],[3,4,5]]
+
+# print(doble[1][2])
+
+import re
+
+pattern = r"'\w*'"
+
+# match = re.search(pattern, "'dsa', 'dsad'")
+ob = re.findall(pattern, "'dsa', 'dsad'")
+print(re.findall(pattern, "'dsa', 'dsad'"))
+# if match:
+# #    print(match.group())
+#    print(match.start())
+#    print(match.end())
+#    print(match.span())
